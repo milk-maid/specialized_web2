8c96e26b5d085adaba662cc4f97089492a37cce0
Bello Roqeeb <roqbell@gmail.com>
2022-07-05 16:34:18 +0100

    na here we dey

---
+++ web2.js
@@ -0,0 +1,51 @@
+
+    document.write("<br>");
+//Ass QUESTION 2 (20marks)
+    let dist = 500;
+    let time = 10;
+    let av_spee
+    av_speed = dist / time;
+    document.write(
+        'The speed of the car is ' + av_speed + 'km/hr'
+        );
+   
+    document.write("<br>")
+
+//Ass QUESTION 3 (5marks)
+    //a=5;
+      a=2;
+    //a=6;
+   document.write(
+       'The output of the code is ' + a + ' because other code lines were commented out.'
+       );
+
+    document.write("<br>")
+    
+// ***TODAY 7/5/2022 CLASSWORK***
+
+   // let mth1 = "June";
+    //  if(mth1 == "August"){
+    //      document.write("I'm ready for vacation");
+    //  } else{
+    //      document.write("...then, I'm not ready");
+    // 
+    // let score = 11;
+    // if(score >= 70 && score <= 100){
+    //     document.write("Excellent");
+    // }
+    // else if (score >= 60 && score <= 69  ){
+    //     document.write("Very Good");
+    // }
+    // else if (score >= 50 && score <= 59){
+    //     document.write("Good");
+    // }
+    // else if (score >= 40 && score <= 49){
+    //     document.write("Fair");
+    // }
+    // else if (score >= 0 && score <= 39 ) {
+    //     document.write("Fail");
+    // }
+    // else {
+    //     document.write("no such grade")
+    // }
+                
